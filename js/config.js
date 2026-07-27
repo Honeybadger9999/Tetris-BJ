96201d81255d559e9b170bef770d81aeedb2992c
BJ <sksqjawn@gmail.com>
2026-07-27 14:14:33 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,8 +5,9 @@
  */
 'use strict';
 
-const SUPABASE_URL = '';       // 예: 'https://xxxxxxxx.supabase.co'
-const SUPABASE_ANON_KEY = '';  // 예: 'eyJhbGciOi...'
+const SUPABASE_URL = 'https://poutdeqosfmktfkatlhw.supabase.co';       // 예: 'https://xxxxxxxx.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvdXRkZXFvc2Zta3Rma2F0bGh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxMDQ0NDIsImV4cCI6MjEwMDY4MDQ0Mn0.9kbz2iGkWyl1N6a1Py1oOZt84JaRCybuAjAgqNL-hhE
+';  // 예: 'eyJhbGciOi...'
 
 const SB = (SUPABASE_URL && SUPABASE_ANON_KEY && window.supabase)
   ? window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
